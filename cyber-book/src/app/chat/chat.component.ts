13cf3c9198029c048e4b89587d57a1c9fb7ece5f
Daniel Cheng, Patrick Napper and Sravy Velaga <patrick@>
2017-12-20 15:33:26 -0800

    updates to chat log

--- cyber-book/src/app/chat/chat.component.ts
+++ cyber-book/src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ChatService } from '../chat.service';
 import { Chat } from '../chat.model';
 import { Router } from '@angular/router';
@@ -11,7 +11,11 @@ import { FirebaseListObservable } from 'angularfire2/database';
   styleUrls: ['./chat.component.css'],
   providers: [ChatService]
 })
+
 export class ChatComponent implements OnInit {
+
+  @Input() childUser;
+
   chats: FirebaseListObservable<any[]>;
   currentRoute: string = this.router.url;
 
@@ -21,8 +25,15 @@ export class ChatComponent implements OnInit {
     this.chats = this.chatService.getChats();
   }
 
-  submitForm(alias: string,title: string) {
-    var newChat: Chat = new Chat(alias, title);
+  // submitForm(title: string) {
+  //   var newChat: Chat = new Chat(this.childUser, title);
+  //   this.chatService.addChat(newChat);
+  // }
+
+  postMessage(event) {
+    event.preventDefault();
+    let message = event.target.elements[0].value;
+    var newChat: Chat = new Chat(this.childUser, message);
     this.chatService.addChat(newChat);
   }
 
